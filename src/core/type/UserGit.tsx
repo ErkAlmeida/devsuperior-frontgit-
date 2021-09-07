2c005cc73e4e744311f53d1e6e4cc13dc66a11d4
Almeida <erick almeida santos>
2021-09-07 00:06:36 -0300

    conclusao da chamado da api git

---
+++ src/core/type/UserGit.tsx
@@ -0,0 +1,12 @@
+export type UserGit ={
+    public_repos?: string,
+    company?: string,
+    blog?: string,
+    location?: string,
+    created_at?: string,
+    followers?: number,
+    following?: number,
+    avatar_url?: string,
+    html_url: string
+
+}
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,20 +1,89 @@
-import React from "react";
+import React, {  useState } from "react";
+import { Link } from "react-router-dom";
 import ButtonP from "../../core/components/ButtonP";
+import { UserGit } from "../../core/type/UserGit";
+import { makeRequest } from "../../core/utils/request";
 import "./styles.scss"
 
-const Profile =() => {
+type FormState = {
+    userName?: string
+}
+
+type FormEvent = React.ChangeEvent<HTMLInputElement>;
+
+const Profile = () => {
+    
+    const [userGit, setUserGit] = useState<UserGit>({
+        followers: 0,
+        avatar_url:'',
+        html_url: ''
+    });
+
+    const [formData, setFormData] = useState<FormState>({
+        userName:''
+    });
+
+    const handleOnChange = (event: FormEvent) => {
+        const userName = event.target.name;
+        const value = event.target.value;
+
+        setFormData(data => ({ ...data, [userName]: value }));
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const payload = {
+            ...formData
+        }
+
+        makeRequest({ url: `/users/${payload.userName}`, method: 'GET'})
+        .then(response => setUserGit(response.data))
+
+    }
 
     return (
-        <div className="home-container">
-           
-            <div className="home-container-form">
-                <h1>Encontre um perfil Github</h1>
-                <input type="text" className="imput-form" placeholder="Usuário Github"></input>
-                <ButtonP text="Encontrar"/>
-            </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <div className="profile-container">
 
-        </div>
-        
+                    <div className="profile-container-form">
+                        <h1>Encontre um perfil Github</h1>
+                        <input 
+                            value={formData.userName}
+                            name="userName"
+                            type="text" 
+                            className="profile-imput-form" 
+                            onChange={handleOnChange}
+                            placeholder="Usuário Github" 
+                        />
+                        <ButtonP text="Encontrar" />
+                    </div>
+                </div>
+                <div className="profile-container-return">
+                    <div className="profile-img">
+                        <img src={userGit?.avatar_url} className="profile-avatar-url" alt="img-git"/>
+                        <Link to={userGit.html_url}>
+                            <ButtonP text="Ver Perfil" />
+                        </Link>   
+                    </div>
+                    <div>
+                        <div className="profile-list-data-title">
+                            <input className="profile-list-data-input-head" type="label" value={`Repositórios públicos: ${userGit.public_repos}`} />
+                            <input className="profile-list-data-input-head" type="label" value={`Seguidores: ${userGit.followers}`} />
+                            <input className="profile-list-data-input-head" type="label" value={`Seguindo: ${userGit.following}`}  />
+                        </div>
+                        <div className="profile-get-data">
+                            <h5 className="title-form">Informações</h5>
+                            <input className="profile-list-data-input" type="label" value={`Empresa: ${userGit.company}`} />
+                            <input className="profile-list-data-input" type="label" value={`Website/Blog: ${userGit.blog}`} />
+                            <input className="profile-list-data-input" type="label" value={`Localidade: ${userGit.location}`} />
+                            <input className="profile-list-data-input" type="label" value={`Membro desde: ${userGit.created_at}`} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     )
 };
 
